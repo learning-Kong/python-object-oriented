ea39a478c3e614edc3718fd0a30a89e3388354c6
kongxianglei <1032805171@qq.com>
2018-08-02 15:32:59 +0800

    try...excpect

---
+++ python-try...excpect.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# AttributeError 试图访问一个对象没有的树形，比如foo.x，但是foo没有属性x
+# IOError 输入/输出异常；基本上是无法打开文件
+# ImportError 无法引入模块或包；基本上是路径问题或名称错误
+# IndentationError 语法错误（的子类） ；代码没有正确对齐
+# IndexError 下标索引超出序列边界，比如当x只有三个元素，却试图访问x[5]
+# KeyError 试图访问字典里不存在的键
+# KeyboardInterrupt Ctrl+C被按下
+# NameError 使用一个还未被赋予对象的变量
+# SyntaxError Python代码非法，代码不能编译(个人认为这是语法错误，写错了）
+# TypeError 传入对象类型与要求的不符合
+# UnboundLocalError 试图访问一个还未被设置的局部变量，基本上是由于另有一个同名的全局变量，
+# 导致你以为正在访问它
+# ValueError 传入一个调用者不期望的值，即使值的类型是正确的
+
+try:
+    f = open('try...excpect.txt','r',encoding='utf-8')
+    print(f.__next__(),end='')
+    print(f.__next__(),end='')
+    print(f.__next__(),end='')
+    print(f.__next__(),end='')
+    print(f.__next__(),end='')
+except Exception:
+    print ('error')
+finally:
+    f.close()           #无论异常是否发生都需要执行
+
+try:
+    print('111111111111')
+    #name
+    print ('22222222222')
+    d = {}
+    #d['name']
+    print ('333333333333')
+    1/0
+except NameError as e:      #elif
+    print ('NameError: ',e)
+except IndexError as e:     #elif
+    print ('IndexError: ',e)
+except KeyError as e:       #elif
+    print ('KeyError: ',e)
+except Exception as e:              #Exception 所有类型的异常都可以检查出来
+    print ('all check error: ',e)
+
+#-------------------------------------------主动触发异常--------------------------------------------
+# 主动触发异常： raise 异常类型（值）
+
+class People:
+    def __init__(self,name,age):
+        if not isinstance(name,str):
+            print ("name type must be string")
+        if not isinstance(age,int):
+            raise TypeError('age must be int')      #自定义，出现异常错误
+
+man = People(123,'wen')
